e16b94a3296de690ad1bba3158a8c2f2379e0c46
Nishant Gupta <shub002gupta@gmail.com>
2020-05-13 12:21:36 +0530

    first commit

---
+++ AddNoteApp WatchKit App/NoteSelectRow.swift
@@ -0,0 +1,13 @@
+//
+//  NoteSelectRow.swift
+//  AddNoteApp WatchKit Extension
+//
+//  Created by Nishant Gupta on 13/05/20.
+//  Copyright © 2020 Nishant Gupta. All rights reserved.
+//
+
+import WatchKit
+
+class NoteSelectRow: NSObject {
+
+}
